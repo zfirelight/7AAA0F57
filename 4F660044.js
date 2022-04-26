230d49db6edc3885198db326d1f09daf0c78e2f9
zfirelight <90018684+zfirelight@users.noreply.github.com>
2022-04-26 02:09:12 -0400

    Add files via upload

--- 4F660044.js
+++ 4F660044.js
@@ -1,26 +1,26 @@
 function myFunction() { 
-    document.getElementById("mp4").src = "https://s3.wasabisys.com/degoo-production-large-file-us-east1.degoo.info/QDWqvl/i6K5Dw/m4a/ChT4IOVS7Tm6UgLqDoY7CN50aMoGeBAA.m4a?AWSAccessKeyId=QCIW8NA9JUUC4PKQYZTJ&Expires=1650808826&Signature=ZXxBU5znYEP0NrYANn6lxa8pfzs%3D";
+    document.getElementById("mp4").src = "https://drive.google.com/uc?export=view&id=12b5IwKDsUFCH_4U5ghW_EkJn4u2dLLgh";
     document.getElementById("video").load();
     document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/1.png";
     document.getElementById("video").addEventListener('ended',myFunction2,false);
 }
 
 function myFunction2() { 
-    document.getElementById("mp4").src = "https://s3.wasabisys.com/degoo-production-large-file-us-east1.degoo.info/QDWqvl/i6K5Dw/m4a/ChSbGlzsoNKF2SQEvVz_1uo4aJr1mxAA.m4a?AWSAccessKeyId=QCIW8NA9JUUC4PKQYZTJ&Expires=1650808869&Signature=C2VjTVah5EF%2B5fhtCXDsUCiRFPc%3D";
+    document.getElementById("mp4").src = "https://drive.google.com/uc?export=view&id=1tGDeji59JH0iLPYjoefxs025PD6b5D01";
     document.getElementById("video").load();
     document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/2.png";
     document.getElementById("video").addEventListener('ended',myFunction3,false);
 }
 
 function myFunction3() { 
-    document.getElementById("mp4").src = "https://s3.wasabisys.com/degoo-production-large-file-us-east1.degoo.info/QDWqvl/i6K5Dw/m4a/ChSGWXmSXQ6213DRFJNKPuA8PKAwNhAA.m4a?AWSAccessKeyId=QCIW8NA9JUUC4PKQYZTJ&Expires=1650808893&Signature=XerBb1%2BJA0U0oFF4Jd0%2FFDa27Lc%3D";
+    document.getElementById("mp4").src = "https://drive.google.com/uc?export=view&id=1pYaLtWWKoaDiw8EhumiuEO_ABlV69_5D";
     document.getElementById("video").load();
     document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/3.png";
     document.getElementById("video").addEventListener('ended',myFunction4,false);
 }
 
 function myFunction4() { 
-    document.getElementById("mp4").src = "https://s3.wasabisys.com/degoo-production-large-file-us-east1.degoo.info/QDWqvl/i6K5Dw/m4a/ChQBCESENJwVV-Y3VpTwZjYC-c_wMxAA.m4a?AWSAccessKeyId=QCIW8NA9JUUC4PKQYZTJ&Expires=1650808929&Signature=S6jJHaB9joDL5p9h9GiRy7LBLl4%3D";
+    document.getElementById("mp4").src = "https://drive.google.com/uc?export=view&id=1xUwQmJLGNci0b5xCgVLej4CgXgjUj7U_";
     document.getElementById("video").load();
     document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/4.png";
     document.getElementById("video").addEventListener('ended',myFunction5,false);
