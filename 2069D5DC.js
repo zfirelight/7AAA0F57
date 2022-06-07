19c9e37f8052c2a9520838b8fca17663c2ef8f3c
zfirelight <90018684+zfirelight@users.noreply.github.com>
2022-06-07 15:21:48 -0400

    Add files via upload

--- 2069D5DC.js
+++ 2069D5DC.js
@@ -1,26 +1,26 @@
 function myFunction() { 
-    document.getElementById("mp4").src = "https://drive.google.com/uc?export=view&id=1yp0MiHFiabOAaskLaAVaeXG-hgukXCC5";
+    document.getElementById("mp4").src = "https://drive.google.com/uc?id=1yp0MiHFiabOAaskLaAVaeXG-hgukXCC5&export=download";
     document.getElementById("video").load();
     document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/1.png";
     document.getElementById("video").addEventListener('ended',myFunction2,false);
 }
 
 function myFunction2() { 
-    document.getElementById("mp4").src = "https://drive.google.com/uc?export=view&id=162zQ7M6-aPRwdVBmBzHUxHPSOR06ft06";
+    document.getElementById("mp4").src = "https://drive.google.com/uc?id=162zQ7M6-aPRwdVBmBzHUxHPSOR06ft06&export=download";
     document.getElementById("video").load();
     document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/2.png";
     document.getElementById("video").addEventListener('ended',myFunction3,false);
 }
 
 function myFunction3() { 
-    document.getElementById("mp4").src = "https://drive.google.com/uc?export=view&id=1vOqjkWfBIPQFXAU7aeIe1HX2jL29PazI";
+    document.getElementById("mp4").src = "https://drive.google.com/uc?id=1vOqjkWfBIPQFXAU7aeIe1HX2jL29PazI&export=download";
     document.getElementById("video").load();
     document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/3.png";
     document.getElementById("video").addEventListener('ended',myFunction4,false);
 }
 
 function myFunction4() { 
-    document.getElementById("mp4").src = "https://drive.google.com/uc?export=view&id=1lcPi_txQ_Smvp_fY-1mUJqJaHFGHYX-a";
+    document.getElementById("mp4").src = "https://drive.google.com/uc?id=1lcPi_txQ_Smvp_fY-1mUJqJaHFGHYX-a&export=download";
     document.getElementById("video").load();
     document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/4.png";
     document.getElementById("video").addEventListener('ended',myFunction5,false);
