047c425ac6cd96a855a7d1d32c5a190238f501bb
zfirelight <90018684+zfirelight@users.noreply.github.com>
2022-06-07 18:39:13 -0400

    Add files via upload

--- 5621B591.js
+++ 5621B591.js
@@ -1,26 +1,26 @@
 function myFunction() { 
-    document.getElementById("mp4").src = "https://drive.google.com/uc?id=19wVmeuXIkGkyJzTxcdqmMhRBn9BO2gnP&export=download";
+    document.getElementById("mp4").src = "https://drive.google.com/uc?id=19wVmeuXIkGkyJzTxcdqmMhRBn9BO2gnP&export=download.m4a";
     document.getElementById("video").load();
     document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/1.png";
     document.getElementById("video").addEventListener('ended',myFunction2,false);
 }
 
 function myFunction2() { 
-    document.getElementById("mp4").src = "https://drive.google.com/uc?id=1n34XYejeaYioGzbgZded7lHUKvg8EEUa&export=download";
+    document.getElementById("mp4").src = "https://drive.google.com/uc?id=1n34XYejeaYioGzbgZded7lHUKvg8EEUa&export=download.m4a";
     document.getElementById("video").load();
     document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/2.png";
     document.getElementById("video").addEventListener('ended',myFunction3,false);
 }
 
 function myFunction3() { 
-    document.getElementById("mp4").src = "https://drive.google.com/uc?id=1_x5AmQWnksy73iOGm1TY70pHLibrLMOC&export=download";
+    document.getElementById("mp4").src = "https://drive.google.com/uc?id=1_x5AmQWnksy73iOGm1TY70pHLibrLMOC&export=download.m4a";
     document.getElementById("video").load();
     document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/3.png";
     document.getElementById("video").addEventListener('ended',myFunction4,false);
 }
 
 function myFunction4() { 
-    document.getElementById("mp4").src = "https://drive.google.com/uc?id=1w-oy1Ox9lG_N1pu1to6SMFtCH8NZ2eop&export=download";
+    document.getElementById("mp4").src = "https://drive.google.com/uc?id=1w-oy1Ox9lG_N1pu1to6SMFtCH8NZ2eop&export=download.m4a";
     document.getElementById("video").load();
     document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/4.png";
     document.getElementById("video").addEventListener('ended',myFunction5,false);
--- 9EC13AE1.js
+++ 9EC13AE1.js
@@ -1,26 +1,26 @@
 function myFunction() { 
-    document.getElementById("mp4").src = "https://drive.google.com/uc?id=1H4Py7g3MBa039k1wkdBcUyMJHVkCypMu&export=download";
+    document.getElementById("mp4").src = "https://drive.google.com/uc?id=1H4Py7g3MBa039k1wkdBcUyMJHVkCypMu&export=download.m4a";
     document.getElementById("video").load();
     document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/1.png";
     document.getElementById("video").addEventListener('ended',myFunction2,false);
 }
 
 function myFunction2() { 
-    document.getElementById("mp4").src = "https://drive.google.com/uc?id=1wsCXkm12JCwpJfD_ItpE9Fol82RZK9hf&export=download";
+    document.getElementById("mp4").src = "https://drive.google.com/uc?id=1wsCXkm12JCwpJfD_ItpE9Fol82RZK9hf&export=download.m4a";
     document.getElementById("video").load();
     document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/2.png";
     document.getElementById("video").addEventListener('ended',myFunction3,false);
 }
 
 function myFunction3() { 
-    document.getElementById("mp4").src = "https://drive.google.com/uc?id=1DnACFONTkq2EJ3Yu_sri_n0e8aFP1n-z&export=download";
+    document.getElementById("mp4").src = "https://drive.google.com/uc?id=1DnACFONTkq2EJ3Yu_sri_n0e8aFP1n-z&export=download.m4a";
     document.getElementById("video").load();
     document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/3.png";
     document.getElementById("video").addEventListener('ended',myFunction4,false);
 }
 
 function myFunction4() { 
-    document.getElementById("mp4").src = "https://drive.google.com/uc?id=1o7LHVo-JV9z6Njl8rOkIhMnihA7rmxPf&export=download";
+    document.getElementById("mp4").src = "https://drive.google.com/uc?id=1o7LHVo-JV9z6Njl8rOkIhMnihA7rmxPf&export=download.m4a";
     document.getElementById("video").load();
     document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/4.png";
     document.getElementById("video").addEventListener('ended',myFunction5,false);
