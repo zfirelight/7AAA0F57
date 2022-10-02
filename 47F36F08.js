7b7127d5f006865c3ce98a0b02755e57a398f55f
zfirelight <90018684+zfirelight@users.noreply.github.com>
2022-10-02 12:03:52 -0400

    Add files via upload

---
+++ 47F36F08.js
@@ -0,0 +1,33 @@
+function myFunction() { 
+    document.getElementById("mp4").src = "https://drive.google.com/uc?id=1rfhZHonAewoQL28nAnMBkMNpq4NyhLxQ&export=download";
+    document.getElementById("video").load();
+    document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/1.png";
+    document.getElementById("video").addEventListener('ended',myFunction2,false);
+}
+
+function myFunction2() { 
+    document.getElementById("mp4").src = "https://drive.google.com/uc?id=1BJJNJMOQ_kvEO3ZGZSrcW-mEiCn5iVJ9&export=download";
+    document.getElementById("video").load();
+    document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/2.png";
+    document.getElementById("video").addEventListener('ended',myFunction3,false);
+}
+
+function myFunction3() { 
+    document.getElementById("mp4").src = "https://drive.google.com/uc?id=1zYJy8iB2p_4daqanIR9CUjcfpyudretq&export=download";
+    document.getElementById("video").load();
+    document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/3.png";
+    document.getElementById("video").addEventListener('ended',myFunction4,false);
+}
+
+function myFunction4() { 
+    document.getElementById("mp4").src = "https://drive.google.com/uc?id=190RERZAlWZsMUCobmyjFKlyaRJcrpasF&export=download";
+    document.getElementById("video").load();
+    document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/4.png";
+    document.getElementById("video").addEventListener('ended',myFunction5,false);
+}
+
+function myFunction5() { 
+    document.getElementById("mp4").src = "";
+    document.getElementById("video").load();
+    document.getElementById("video").poster = "https://sites.google.com/site/zfirelight/refresh.png";
+}
